ef1cd0afc49a595660ff6f2ebbb23fcc6a088220
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-04-10 22:34:45 -0300

    feat: adding welcome page, project configs(colors, nexUI)

---
+++ src/components/Icon.tsx
@@ -0,0 +1,10 @@
+
+export function Icon() {
+    return (
+        <div className="flex items-center p-0" >
+            <img src="/barberpole.png" alt="barber pole" className="w-16 h-16 m-0"/>
+            <h1 className="text-stone-950 text-5xl font-bold m-0">Barber<span className="text-cerulean-blue-700 font-bold">Time</span></h1>
+        </div>  
+    )
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        'cerulean-blue': {
+          50: '#eef8ff',
+          100: '#d9edff',
+          200: '#bce1ff',
+          300: '#8ecfff',
+          400: '#58b3ff',
+          500: '#3192ff',
+          600: '#1b73f5',
+          700: '#135add',
+          800: '#174ab6',
+          900: '#184290',
+          950: '#142957'
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
+
