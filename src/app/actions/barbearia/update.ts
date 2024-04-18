6fd63a639761314641f8c7bdd8230d000675e806
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-04-17 23:04:17 -0300

    feat: adding get

---
+++ src/app/actions/barbearia/update.ts
@@ -0,0 +1,43 @@
+"use server"
+
+import { redirect } from "next/navigation"
+
+export async function update(prevState: any, formData: FormData){
+    await new Promise(r => setTimeout(r, 1000))
+
+    const id = formData.get("id")
+    
+    const data = {
+      nome: formData.get("nome"),
+      email: formData.get("email"),
+      telefone: formData.get("tel"),
+      cnpj: formData.get("cnpj")
+  }
+
+    const options = {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    const resp = await fetch(`${process.env.API_BASE_URL}/barbearia/${id}`, options)
+    
+    if (resp.ok){
+      redirect("/barbearia")
+    }
+
+    if (resp.status == 400){
+      return {
+        messageNome: "Validação falhou"
+      }
+    }
+
+    if (resp.status == 404){
+      return {
+        messageNome: "Categoria não encontrada"
+      }
+    }
+    
+  }
--- src/app/home/barbearia/page.jsx
+++ src/app/home/barbearia/page.jsx
@@ -1,11 +1,32 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 
+export default function Home() {
+  const [dados, setDados] = useState([]);
 
-export default async function Home() {
-    return (
-      <main>
-        <h2>Barbearia</h2>
-      </main>
-    );
-  }
+  useEffect(() => {
+    fetch('http://localhost:8080/barbearia')
+      .then((response) => response.json())
+      .then((data) => setDados(data));
+  }, []);
+
+  return (
+    <main>
+      <h2>Barbearia</h2>
+      {dados.length > 0 ? (
+        <ul>
+          {dados.map((item) => (
+            <li key={item.id}>
+              Nome: {item.nome} <br />
+              Email: {item.email} <br />
+              Telefone: {item.telefone} <br />
+              CNPJ: {item.cnpj} <br />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Carregando dados...</p>
+      )}
+    </main>
+  );
+}
